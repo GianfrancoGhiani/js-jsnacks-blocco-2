8ad0bf848a5f23640f3df4a25bfcf83f18adae98
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2022-10-12 16:38:06 +0200

    snack2

--- js/script.js
+++ js/script.js
@@ -5,13 +5,15 @@
 Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 
 **Snack2**
-Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
+Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi,
+e da queste vuole generare una falsa lista di invitati con nome e cognome.
 
 **Snack3**
 Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione dispari
 
 **Snack4 (Bonus)**
-Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all’array che ha meno elementi fino a quando
+ne avrà tanti quanti l’altro.
 */
 
 
@@ -39,3 +41,30 @@ button.addEventListener('click', function printingEven(){
     showNum.innerText = num;
     number.value = '';
 } )
+
+//  snack2
+const nameList = ['Leonardo', 'Francesco', 'Alessandro', 'Lorenzo', 'Mattia', 'Tommaso', 'Gabriele', 'Andrea', 'Sofia', 'Giulia', 'Aurora', 'Alice', 'Ginevra', 'Emma', 'Giorgia'];
+const surnList = ['Rossi', 'Ferrari', 'Russo', 'Bianchi', 'Esposito', 'Colombo', 'Romano', 'Ricci', 'Gallo', 'Greco', 'Conti', 'Marino', 'De Luca', 'Bruno', 'Costa']
+const invitations = [];
+
+const button2 = snack2.querySelector('button');
+
+
+button2.addEventListener('click', function createInvitations(){
+
+        let listLine = 0;
+    
+    for (let i = 0; i < nameList.length; i++){
+        let n = Math.floor(Math.random() * (14 - 0 + 1) ) + 0;
+        let s = Math.floor(Math.random() * (14 - 0 + 1) ) + 0;
+        invitations.push(`${nameList[n]} ${surnList[s]}`);
+        listLine = document.createElement('p');
+        listLine.classList.add('w-25');
+        listLine.classList.add('border-bottom-1');
+        snack2.querySelector('div').append(listLine);
+        listLine.innerText = `${invitations[i]}`;
+    }
+    
+})
+
+
