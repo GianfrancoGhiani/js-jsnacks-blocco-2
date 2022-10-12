ee9c18abc4fdd45e0ab1ae62873d6aeebcd846fa
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2022-10-12 17:14:32 +0200

    comments added

--- js/script.js
+++ js/script.js
@@ -33,42 +33,51 @@ showNum.classList.add('ms-4');
 snack1.append(showNum);
 
 button.addEventListener('click', function printingEven(){
+    // pulizia da numeri precedentemente visualizzati
     showNum.innerText = '';
+    // controllo campo non vuoto
     if(!(number.value == '')){
         let num = parseInt(number.value);
         showNum.innerText = '';
+        // se dispari darà resto -->   not(not(resto)) = true
         if (!(!(num % 2))){
             num++;
         }
         showNum.innerText = num;
-        
     }
     number.value = '';
     
 } )
 
 //  snack2
+
+// creo 2 liste: nomi e cognomi
+
 const nameList = ['Leonardo', 'Francesco', 'Alessandro', 'Lorenzo', 'Mattia', 'Tommaso', 'Gabriele', 'Andrea', 'Sofia', 'Giulia', 'Aurora', 'Alice', 'Ginevra', 'Emma', 'Giorgia'];
 const surnList = ['Rossi', 'Ferrari', 'Russo', 'Bianchi', 'Esposito', 'Colombo', 'Romano', 'Ricci', 'Gallo', 'Greco', 'Conti', 'Marino', 'De Luca', 'Bruno', 'Costa']
 const invitations = [];
 
+const div2 = snack2.querySelector('div');
 const button2 = snack2.querySelector('button');
 
 
 button2.addEventListener('click', function createInvitations(){
-
-        let listLine = 0;
-    
-    for (let i = 0; i < nameList.length; i++){
+    // pulisco la lista "zona" della pagina di visualizzazione lista in caso sia già piena da cicli precedenti
+    div2.innerHTML= ''; 
+    // pulisco la lista invitati in caso sia già piena da cicli precedenti (liste sempre diverse)
+    for (let i = 0; i < nameList.length; i++){ 
+        invitations.splice(0, 1);
+    }
+    // unisco randomicamente le 2 liste in una terza lista "invitati" e stampo su pagina
+    for (let i = 0; i < nameList.length; i++){ 
         let n = Math.floor(Math.random() * (14 - 0 + 1) ) + 0;
         let s = Math.floor(Math.random() * (14 - 0 + 1) ) + 0;
         invitations.push(`${nameList[n]} ${surnList[s]}`);
-        listLine = document.createElement('p');
+        let listLine = document.createElement('p');
         listLine.classList.add('col-4');
         snack2.querySelector('div').append(listLine);
         listLine.innerText = `${invitations[i]}`;
     }
-    
 })
 
-
+// snack3
